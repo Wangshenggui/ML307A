bbf59f8e1946b9d307731a885134a500900e73ca
Wangshenggui <1785193871@qq.com>
2024-06-19 16:24:13 +0800

    添加RMC解析函数

--- custom/custom_main/inc/nema.h
+++ custom/custom_main/inc/nema.h
@@ -25,8 +25,41 @@ typedef struct
 } GPGGA_Structure;
 extern GPGGA_Structure GPGGA_Struct;
 
+typedef struct
+{
+    char Buff[200];
+
+    char str[100];
+    uint8_t Hour;
+    uint8_t Min;
+    uint8_t Sec;
+
+    uint16_t Year;
+    uint8_t Month;
+    uint8_t Day;
+
+    uint16_t LatitudeDegrees;
+    uint8_t LatitudeMinutes;
+    uint8_t LatitudeSeconds;
+
+    uint16_t LongitudeDegrees;
+    uint8_t LongitudeMinutes;
+    uint8_t LongitudeSeconds;
+
+    float Speed;
+    float CourseAngle;
+
+    uint8_t ModeIndicator;
+    uint8_t LocationState;
+
+    uint8_t LatitudeDir;
+    uint8_t LongitudeDir;
+} GPRMC_Structure;
+extern GPRMC_Structure GPRMC_Struct;
+
 int extractFirstxxx(const char *data, char *result, size_t result_size,char *gga_start);
 void ParseGPGGA(const char *string, int n);
+void ParseGPRMC(const char *string, int n);
 
 
 #endif
--- custom/custom_main/src/nema.c
+++ custom/custom_main/src/nema.c
@@ -6,6 +6,7 @@
 uint8_t GGAString[100];
 
 GPGGA_Structure GPGGA_Struct;
+GPRMC_Structure GPRMC_Struct;
 
 #define MAX_LINE_LENGTH 256
 
@@ -53,11 +54,10 @@ int extractFirstxxx(const char *data, char *result, size_t result_size, char *gg
     return 0; // 没有找到 "$GNGGA" 行
 }
 
-
-//裁剪字符串
-static char* substring(char* dst, char* src, int start, int len)
+// 裁剪字符串
+static char *substring(char *dst, char *src, int start, int len)
 {
-    int length = 1000;//×î´ó³¤¶È
+    int length = 1000; // ×î´ó³¤¶È
     if (start >= length || start < 0)
         return NULL;
     if (len > length)
@@ -159,6 +159,166 @@ void ParseGPGGA(const char *string, int n)
         case (9): // 海拔高度
             substring(GPGGA_Struct.str, result, 0, strlen(result));
             GPGGA_Struct.Height = atof(GPGGA_Struct.str);
+            break;
+        }
+    }
+}
+
+void ParseGPRMC(const char *string, int n)
+{
+    char result[200];
+    unsigned char max_length = 200;
+    const char *start = string;
+
+    int xor_sum = 0; // 校验值
+    int i = 0;
+    unsigned char len = 0; // 计算字符串长度
+    char checksum_str[3];
+
+    // 计算长度
+    while (string[len] != '\0')
+        len++;
+    // 计算数据的异或和
+    for (i = 1; i < len; i++)
+    {
+        if (string[i] == '*')
+            break;
+        xor_sum ^= string[i];
+    }
+    sprintf(checksum_str, "%02X", xor_sum);
+    if (strncmp(checksum_str, (const char *)&string[i + 1], 2) == 0) // 通过
+    {
+        // 定位到第n个逗号
+        for (int i = 0; i < n; i++)
+        {
+            start = strchr(start, ',');
+            if (!start)
+            {
+                // 如果逗号不够n个，将结果设为空字符串
+                result[0] = '\0';
+                return;
+            }
+
+            // 移动到下一个字符
+            start++;
+        }
+
+        // 计算逗号后字符串的长度
+        const char *end = strchr(start, ',');
+        size_t length = end ? (size_t)(end - start) : strlen(start);
+
+        // 截取字符串并复制到结果
+        if (length < max_length - 1)
+        {
+            strncpy(result, start, length);
+            // 手动添加 null 结尾
+            result[length] = '\0';
+        }
+        else
+        {
+            // 目标长度不足，截断字符串
+            strncpy(result, start, max_length - 1);
+            result[max_length - 1] = '\0';
+        }
+
+        switch (n)
+        {
+        case (1): // 时间
+            // 解析时间
+            substring(GPRMC_Struct.str, result, 0, 2);
+            GPRMC_Struct.Hour = (uint8_t)atof(GPRMC_Struct.str) + 8;
+            substring(GPRMC_Struct.str, result, 2, 2);
+            GPRMC_Struct.Min = (uint8_t)atof(GPRMC_Struct.str);
+            substring(GPRMC_Struct.str, result, 4, 2);
+            GPRMC_Struct.Sec = (uint8_t)atof(GPRMC_Struct.str);
+            break;
+        case (2): // 定位状态
+            GPRMC_Struct.LocationState = result[0];
+            break;
+        case (3): // 纬度
+            // 解析纬度
+            // 度
+            substring(GPRMC_Struct.str, result, 0, 2);
+            GPRMC_Struct.LatitudeDegrees = (uint8_t)atoi(GPRMC_Struct.str);
+            // 分
+            substring(GPRMC_Struct.str, result, 2, 2);
+            GPRMC_Struct.LatitudeMinutes = (uint8_t)atoi(GPRMC_Struct.str);
+            /*GPS*/
+            if (strlen(result) == 11)
+            {
+                // 秒
+                substring(GPRMC_Struct.str, result, 5, 6);
+                GPRMC_Struct.LatitudeSeconds = (uint8_t)(atof(GPRMC_Struct.str) * 0.000001 * 60 + 0.5);
+            }
+            else if (strlen(result) == 9)
+            {
+                // 秒
+                substring(GPRMC_Struct.str, result, 5, 4);
+                GPRMC_Struct.LatitudeSeconds = (uint8_t)(atof(GPRMC_Struct.str) * 0.0001 * 60 + 0.5);
+            }
+            /*RTK*/
+            else if (strlen(result) == 13)
+            {
+                // 秒
+                substring(GPRMC_Struct.str, result, 5, 8);
+                GPRMC_Struct.LatitudeSeconds = (uint8_t)(atof(GPRMC_Struct.str) * 0.00000001 * 60 + 0.5);
+            }
+
+            break;
+        case (4): // N/S
+            GPRMC_Struct.LatitudeDir = result[0];
+            break;
+        case (5): // 经度
+            // 解析经度
+            // 度
+            substring(GPRMC_Struct.str, result, 0, 3);
+            GPRMC_Struct.LongitudeDegrees = (uint8_t)atoi(GPRMC_Struct.str);
+            // 分
+            substring(GPRMC_Struct.str, result, 3, 2);
+            GPRMC_Struct.LongitudeMinutes = (uint8_t)atoi(GPRMC_Struct.str);
+
+            /*GPS*/
+            if (strlen(result) == 11)
+            {
+                // 秒
+                substring(GPRMC_Struct.str, result, 6, 6);
+                GPRMC_Struct.LongitudeSeconds = (uint8_t)(atof(GPRMC_Struct.str) * 0.000001 * 60 + 0.5);
+            }
+            else if (strlen(result) == 9)
+            {
+                // 秒
+                substring(GPRMC_Struct.str, result, 6, 4);
+                GPRMC_Struct.LongitudeSeconds = (uint8_t)(atof(GPRMC_Struct.str) * 0.0001 * 60 + 0.5);
+            }
+            /*RTK*/
+            else if (strlen(result) == 14)
+            {
+                // 秒
+                substring(GPRMC_Struct.str, result, 6, 8);
+                GPRMC_Struct.LongitudeSeconds = (uint8_t)(atof(GPRMC_Struct.str) * 0.00000001 * 60 + 0.5);
+            }
+            break;
+        case (6): // E/W
+            GPRMC_Struct.LongitudeDir = result[0];
+            break;
+        case (7): // 地面速度
+            //
+            GPRMC_Struct.Speed = (float)atof(result) * 1.852;
+            break;
+        case (8): // 航向角
+            GPRMC_Struct.CourseAngle = (float)atof(result);
+            break;
+        case (9): // 日期
+            // 解析日期
+            substring(GPRMC_Struct.str, result, 0, 2);
+            GPRMC_Struct.Day = (uint8_t)atof(GPRMC_Struct.str);
+            substring(GPRMC_Struct.str, result, 2, 2);
+            GPRMC_Struct.Month = (uint8_t)atof(GPRMC_Struct.str);
+            substring(GPRMC_Struct.str, result, 4, 2);
+            GPRMC_Struct.Year = (uint8_t)atof(GPRMC_Struct.str) + 2000;
+            break;
+        case (12): // 定位模式
+            GPRMC_Struct.ModeIndicator = result[0];
             break;
         }
     }
